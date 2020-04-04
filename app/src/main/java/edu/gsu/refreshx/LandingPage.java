e1e16521002b9cd0c68dbdf782294dc9919fdc9a
Hadia Jones <tavvion@gmail.com>
2020-04-04 17:02:25 -0400

    landing page added

---
+++ app/src/main/java/edu/gsu/refreshx/LandingPage.java
@@ -0,0 +1,16 @@
+package edu.gsu.refreshx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class LandingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+    }
+}
