34b1b243d6d97b6fcd634aa6d925e5a1c49d2160
Hadia Jones <tavvion@gmail.com>
2020-04-12 12:08:20 -0400

    register button click added

--- app/src/main/java/edu/gsu/refreshx/LandingPage.java
+++ app/src/main/java/edu/gsu/refreshx/LandingPage.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class LandingPage extends AppCompatActivity {
@@ -12,5 +13,12 @@ public class LandingPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+    }
+
+    public void register (View view) {
+        Intent i = new Intent(LandingPage.this, RegisterActivity.class);
+        startActivity(i);
+
+        finish();
     }
 }
