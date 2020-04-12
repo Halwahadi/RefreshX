c956e1a7abb4cba0314212077e109d5901dc76d7
Hadia Jones <tavvion@gmail.com>
2020-04-12 12:08:57 -0400

    registration UI added

--- app/src/main/java/edu/gsu/refreshx/RegisterActivity.java
+++ app/src/main/java/edu/gsu/refreshx/RegisterActivity.java
@@ -2,7 +2,11 @@ package edu.gsu.refreshx;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(RegisterActivity.this, LandingPage.class);
+                startActivity(i);
+
+                finish();
+            }
+        });
     }
 }
