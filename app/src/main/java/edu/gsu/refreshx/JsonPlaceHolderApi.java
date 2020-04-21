8404e02a435fccfb2bce71d8976dc470fa39dad3
nghoangtu <48893744+nghoangtu@users.noreply.github.com>
2020-04-21 16:07:21 -0400

    Update JsonPlaceHolderApi.java

--- app/src/main/java/edu/gsu/refreshx/JsonPlaceHolderApi.java
+++ app/src/main/java/edu/gsu/refreshx/JsonPlaceHolderApi.java
@@ -14,4 +14,6 @@ public interface JsonPlaceHolderApi {
     @GET("/getdrink")
     Call <ApiResult> getdrink(@Query("id") int id, @Query("dname") String dname, @Query("recipe") String recipe,
                               @Query("warnings") String warnings, @Query("website") String website, @Query("image") String image);
+    @GET("/addfavo")
+    Call <ApiResult> addFavorite(@Query("user_id") int user_id, @Query("drink_id") String drink_id);
 }
